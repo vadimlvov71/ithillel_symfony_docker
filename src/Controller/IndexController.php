7f682fd36a8977666088c20d13e0bf5f3ca680f5
Vadim Podolyan <vadim.podolyan@gmail.com>
2024-05-21 12:24:02 +0300

    clear code

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,23 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-/*
-use App\Repository\TaskRepository;
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
-use Monolog\DateTimeImmutable;
-use App\Entity\Status;
-use App\Entity\User;
-use App\Entity\Task;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use App\Service\Validation;
-use App\Service\TasksTree;
-*/
+
 /**
- * ApiController uses OpenAPI and has actions:
- * 4 ones for crud, one - for changing only one field: 'current salary'
- * used API with five entrypoints
- * create a tree of a tasks of a one
+ * ithillel symfony docker home task
  *
  *
 * @author Vadim Podolyan <vadim.podolyan@gmail.com>
